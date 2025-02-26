d25055de401f20bade093d9ee603485f0af8b05c
Steel Ferguson <steel_ferguson@yahoo.com>
2025-02-26 14:00:11 -0600

    Add script to fetch historical crypto data

---
+++ fetch_data.py
@@ -0,0 +1,14 @@
+import ccxt
+import pandas as pd
+
+#initialize connection with Kraken
+exchange = ccxt.kraken()
+symbol = "BTC/USDT"
+
+# Open High Low Close Volume (OHLCV)
+ohlcv = exchange.fetch_phlcv(symbol, "1h", limit=100)
+
+df = pd.DataFrame(ohlcv, columns=["timestamp", "open", "high", "low", "close", "volume"])
+df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+
+print(df.head())
